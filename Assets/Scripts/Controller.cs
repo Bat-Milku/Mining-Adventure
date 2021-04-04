77cfc722ffc942c71d6117098ab65da65ddf3505
Bat-Milku <ivanbt007@gmail.com>
2021-04-04 17:38:08 +0300

    First commit
    i NEED more ram

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+  public Game game;
+  public SpriteRenderer sr;
+  public Rigidbody2D rb;
+  public Animator anim;
+  public Camera cam;
+  public float speed = 10;
+  public float MaxVelocity;
+  public Vector2 jumpForce = new Vector2(0, 10);
+  bool isGrounded = false;
+  public Text debug;
+  public Text debug2;
+  public TileType[] room;
+
+  float digAnimTime = 0;
+  int toDigX, toDigY;
+
+  // Update is called once per frame
+  void Update() {
+    Vector2 force = Vector2.zero;
+    float x = Input.GetAxis("Horizontal");
+    force.x = x * speed;
+    rb.AddForce(force);
+    if (x < 0) sr.flipX = true;
+    else if (x > 0) sr.flipX = false;
+    if (x == 0) {
+      rb.velocity *= .9f;
+    }
+
+    if (digAnimTime > 0) {
+      digAnimTime -= Time.deltaTime;
+      if (digAnimTime <= 0) {
+        game.Dig(toDigX, toDigY);
+      }
+    }
+    else if (rb.velocity.x > .2f || rb.velocity.x < -.2f) {
+      anim.SetInteger("Dig", 0);
+      anim.SetBool("Walking", true);
+    }
+    else {
+      anim.SetInteger("Dig", 0);
+      anim.SetBool("Walking", false);
+    }
+
+    if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+      rb.AddForce(jumpForce);
+      isGrounded = false;
+    }
+
+    Vector2Int pos = Vector2Int.zero;
+    pos.x = (int)transform.position.x - 1;
+    pos.y = -(int)transform.position.y;
+
+    Vector2 vel = rb.velocity;
+    if (vel.x > MaxVelocity) vel.x = MaxVelocity;
+    if (vel.x < -MaxVelocity) vel.x = -MaxVelocity;
+    rb.velocity = vel;
+
+    Vector2 mouse = cam.ScreenToWorldPoint(Input.mousePosition);
+    Dir dir;
+    if (Mathf.Abs(mouse.y - transform.position.y) < .7f) {
+      if (mouse.x > transform.position.x + .25f) dir = Dir.Right;
+      else if (mouse.x < transform.position.x - .25f) dir = Dir.Left;
+      else dir = Dir.None;
+    }
+    else if (Mathf.Abs(mouse.x - transform.position.x) < .7f) {
+      if (mouse.y < transform.position.y - .25f) dir = Dir.Down;
+      else if (mouse.y > transform.position.y + .25f) dir = Dir.Up;
+      else dir = Dir.None;
+    }
+    else dir = Dir.None;
+
+    int px = (int)(transform.position.x + .5f);
+    int py = -Mathf.RoundToInt(transform.position.y - 0f);
+    debug.text = px + "," + py;
+
+    if (dir != Dir.None) {
+
+      if (dir == Dir.Right) px++;
+      if (dir == Dir.Left) px--;
+      if (dir == Dir.Up) py--;
+      if (dir == Dir.Down) py++;
+
+      if (px < 0 || px >= game.gridWidth || py < 0 || py >= game.gridHeight)
+        debug2.text = px + ", " + py + " : " + dir.ToString();
+      else {
+        TileType tile = game.room[px + game.gridWidth * py];
+        if (tile != TileType.Empty && tile != TileType.Wall && Input.GetMouseButtonDown(0)) {
+          anim.SetInteger("Dig", 2);
+          toDigX = px; // To remember where to dig
+          toDigY = py;
+          digAnimTime = .8f;
+          sr.flipX = dir == Dir.Left;
+        }
+        debug2.text = px + ", " + py + " : " + dir + " / " + game.room[px + game.gridWidth * py];
+      }
+    }
+    else {
+      debug2.text = "";
+    }
+    debug.text += " -> " + px + "," + py;
+
+  }
+  void OnCollisionEnter2D(Collision2D collision) {
+    isGrounded = true;
+  }
+
+  public enum Dir { None, Up, Down, Left, Right }
+}
+
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public int gridWidth = 24;
+    public int gridHeight = 16;
+    public TileType[] room = null; 
+    public Sprite[] TileImages;
+    public GameObject tilePrefab;
+    private GameObject[] cells = null;
+    public float numRocks = .1f;
+    public float numGems = .05f;
+    public Transform Room;
+    public Controller player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        room = new TileType[gridWidth * gridHeight];
+        cells = new GameObject[gridWidth * gridHeight];
+        Generate();
+    }
+
+    void Generate()
+    {
+        foreach (Transform t in Room)
+            Destroy(t.gameObject);
+
+        // Generate the walls
+
+        for (int y = 0; y < gridHeight; y++)
+        {
+            room[0 + gridWidth * y] = TileType.Wall;
+            room[(gridWidth - 1) + gridWidth * y] = TileType.Wall;
+        }
+        for (int x = 0; x < gridWidth; x++)
+        {
+            room[x + gridWidth * (gridHeight - 1)] = TileType.Wall;
+        }
+        for (int x = 1; x < gridWidth - 1; x++)
+        {
+            for (int y = 0; y < gridHeight - 1; y++)
+            {
+                room[x + gridWidth * y] = TileType.Dirt;
+            }
+        }
+
+        // Generate Rocks
+        int num = (int)((gridWidth - 2) * gridHeight * numRocks);
+        for (int i = 0; i < num; i++)
+        {
+            int x = Random.Range(1, gridWidth - 1);
+            int y = Random.Range(0, gridHeight - 1);
+            room[x + gridWidth * y] = TileType.Rock;
+        }
+
+        // Generate gems
+
+        num = (int)((gridWidth - 2) * gridHeight * numGems);
+        for (int i = 0; i < num; i++)
+        {
+            int x = Random.Range(1, gridWidth - 1);
+            int y = Random.Range(0, gridHeight - 1);
+            room[x + gridWidth * y] = TileType.Gem;
+        }
+
+        // Draw the room
+
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                int pos = x + gridWidth * y;
+                GameObject tile = Instantiate(tilePrefab, Room);
+                cells[pos] = tile;
+                tile.GetComponent<SpriteRenderer>().sprite = TileImages[(int)room[pos]];
+                tile.transform.position = new Vector3(x, -y, 0);
+                byte col = (byte)(255 - y * 12);
+                if (x > 0 && x < gridWidth - 1 && y < gridHeight - 1) tile.GetComponent<SpriteRenderer>().color = new Color32(col, col, col, 255);
+            }
+        }
+        
+
+        player.transform.position = new Vector3(gridWidth / 2, 2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape)) {
+            Generate();
+        }
+    }
+    public TileType Dig(int x, int y)
+    {
+        if (x < 0 || x >= gridWidth || y < 0 || y >= gridHeight) return TileType.Empty;
+
+        TileType res = room[x + gridWidth * y];
+        if (res == TileType.Empty) return TileType.Empty;
+        room[x + gridWidth * y] = TileType.Empty;
+        Destroy(cells[x + gridWidth * y]);
+        cells[x + gridWidth * y] = null;
+        return res;
+    }
+}
+public enum TileType
+{
+    Empty = 0,
+    Wall = 1,
+    Rock = 2,
+    Gem = 3,
+    Dirt = 4,
+}
